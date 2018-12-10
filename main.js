0df5c32400e53b8f05f554cc8e751ccee0980e1c
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2018-12-10 13:53:49 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function clockUpdate() {
+    const fullDate = new Date();
+
+    let secs = fullDate.getSeconds();
+    let hours = fullDate.getHours();
+    let mins = fullDate.getMinutes();
+    
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (mins < 10) {
+        mins = "0" + mins;
+    }
+    if (secs < 10) {
+        secs = "0" + secs;
+    }
+    document.getElementById('hour').innerHTML = hours;
+    document.getElementById('minute').innerHTML = mins;
+    document.getElementById('second').innerHTML = secs;
+    
+}
+
+setInterval(clockUpdate, 100);
